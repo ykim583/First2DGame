020542a32cb35b15627e7fc86f0d7e6ebe1cc508
Youngmin <ykim583@aucklanduni.ac.nz>
2022-06-25 02:19:12 +1200

    Established and start building the 2D Game, set up the screen display and working on player's motion/Display

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,7 +3,7 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel { // The class to set up screen
+public class GamePanel extends JPanel implements Runnable{ // The class to set up screen
 
     // Screen settings 
     final int originalTileSize = 16; //  16 x 16 tile is created
@@ -15,11 +15,31 @@ public class GamePanel extends JPanel { // The class to set up screen
     final int screenWidth = tileSize * maxScreenCol; // 48 x 16 = 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 48 x 12 = 576 pixels
 
+    Thread gameThread; // This keeps the program running until it exit the game
+
     // Now creating a constructor of gamePanel
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight)); // Set the size of this class
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); // If set to true, (Game rendering performance improved)
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this); // Create new instance of Thread
+        gameThread.start(); // Start this thread
+    }
+
+    @Override
+    public void run() { // When gameThread being called, run method is automatically called
+
+        while(gameThread != null) {
+            System.out.println("The game loop is running"); // Continous game loop processing
+        }
+
+        // We need to update the Player's movement
+        // We also need to draw the screen with the updated information 
+        
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Time needs to run in the game as real life
+        // Video game is like a fast changing images
+        // FPS 60 = The image change sixty seconds after.
         
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 'x' button click, the window is closed
@@ -15,5 +19,7 @@ public class Main {
 
         window.setLocationRelativeTo(null); // The window is displayed at centre of screen.
         window.setVisible(true); // The game window is visible to the developer
+
+        gamePanel.startGameThread();
     }
 }
