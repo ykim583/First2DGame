97febce25d8bec23c9551a0addafeaa210dcd28c
Youngmin <ykim583@aucklanduni.ac.nz>
2022-06-30 23:00:45 +1200

    Player needs to read correct package file in order to load correct image

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -40,14 +40,14 @@ public class Player extends Entity {
 
         try {
 
-            up1 = ImageIO.read(getClass().getResourceAsStream("/res/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/res/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/res/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/res/boy_down_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/res/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/res/boy_left_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/res/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/res/boy_right_2.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/character/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/character/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/character/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/character/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/character/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/character/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/character/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/character/boy_right_2.png"));
 
         } catch (IOException e) {
             e.printStackTrace();
