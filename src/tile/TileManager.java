81c236547b2071f6414b8c70a489cdf4e422a84b
Youngmin <ykim583@aucklanduni.ac.nz>
2022-07-05 17:41:10 +1200

    Depending on the character's fixed location on centre of screen and somewhere at the fixed world map positon, camera will now charcter while it moves along the map.

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -23,10 +23,10 @@ public class TileManager {
 
         tile = new Tile[10];
 
-        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow]; // mapTileNum array will store all the value for Map.txt
+        mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow]; // mapTileNum array will store all the value for Map.txt
 
         getTileImage();
-        loadMap("/res/maps/Map.txt");
+        loadMap("/res/maps/WorldMap.txt");
         ;
     }
 
@@ -43,6 +43,15 @@ public class TileManager {
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(getClass().getResourceAsStream("/res/tile/water.png"));
 
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/res/tile/earth.png"));
+
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/res/tile/tree.png"));
+
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/res/tile/sand.png"));
+
         } catch (IOException e) {
             e.printStackTrace();
 
@@ -54,16 +63,16 @@ public class TileManager {
                                            // loadMap method
 
         try {
-            InputStream is = getClass().getResourceAsStream("/res/maps/Map.txt");
+            InputStream is = getClass().getResourceAsStream(filePath);
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
             int col = 0;
             int row = 0;
 
-            while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+            while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
                 String line = br.readLine();
 
-                while (col < gp.maxScreenCol) {
+                while (col < gp.maxWorldCol) {
                     String numbers[] = line.split(" "); // Read String
 
                     int num = Integer.parseInt(numbers[col]); // Convert To Integer
@@ -72,7 +81,7 @@ public class TileManager {
                     col++;
                 }
 
-                if (col == gp.maxScreenCol) {
+                if (col == gp.maxWorldCol) {
                     col = 0;
                     row++;
                 }
@@ -84,27 +93,38 @@ public class TileManager {
 
     public void draw(Graphics2D g2) {
 
-        int col = 0;
-        int row = 0;
-        int x = 0;
-        int y = 0;
+        // Initally, The map is just set to one fixed portion of the game
+        // Now, we're updating to bigger world map, so setting the camera motion that
+        // follows the character
 
-        while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+        int worldCol = 0;
+        int worldRow = 0;
 
-            int tileNum = mapTileNum[col][row]; // Everything is stored in the map tile Num array now
+        while (worldCol < gp.maxWorldCol && worldRow < gp.maxWorldRow) {
 
-            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null); // 보면 tile[tileNum].image 가 더이상
-                                                                                     // fixed value 아니고 새로주어진거기때문에 다양한
-                                                                                     // 타일 모양 변형 가능
+            int tileNum = mapTileNum[worldCol][worldRow]; // Everything is stored in the map tile Num array now
 
-            col++;
-            x += gp.tileSize;
+            int worldX = worldCol * gp.tileSize; // Step 1 : Capture the position of player
+            int worldY = worldRow * gp.tileSize;
+            int screenX = worldX - gp.player.worldX + gp.player.screenX; // Step 2 : New Screen image display depending
+                                                                         // on charcter position
+            int screenY = worldY - gp.player.worldY + gp.player.screenY; // Step 3 : Since charcter always centred on
+                                                                         // screen,
+            // There's exception when character might start from position (0,0), and blank
+            // space appeared, + gp.player.screen.. offsets this
 
-            if (col == gp.maxScreenCol) { // When it reaches the maximum col, reset and go to next row and continue
-                col = 0;
-                x = 0;
-                row++;
-                y += gp.tileSize;
+            g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null); // 보면
+                                                                                                 // tile[tileNum].image
+                                                                                                 // 가 더이상
+            // fixed value 아니고 새로주어진거기때문에 다양한
+            // 타일 모양 변형 가능
+
+            worldCol++;
+
+            if (worldCol == gp.maxWorldCol) { // When it reaches the maximum col, reset and go to next row and continue
+                worldCol = 0;
+                worldRow++;
+
             }
         }
     }
