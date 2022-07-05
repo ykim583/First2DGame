546b496fbad6ada75dd4a962651990df7f215876
Youngmin <ykim583@aucklanduni.ac.nz>
2022-07-05 17:35:30 +1200

    Now, character is not just constrained on one portion of game part, we need to extend our current variable to world variable

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -6,7 +6,7 @@ import java.awt.image.BufferedImage;
 
 public class Entity {
 
-    public int x, y;
+    public int worldX, worldY; // Indiacates the different co-ordinates (Mechanism of Game Camera)
     public int speed;
 
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
